5d548bc4673cab44c0a358286c92cc6642239487
yimoxiamu <2959623954@qq.com>
2018-07-23 15:04:29 +0800

    xiamu first

---
+++ src/main/java/com/yimoxiamu/webhunter/WebHunterApplication.java
@@ -0,0 +1,12 @@
+package com.yimoxiamu.webhunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHunterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebHunterApplication.class, args);
+    }
+}
---
+++ src/test/java/com/yimoxiamu/webhunter/WebHunterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yimoxiamu.webhunter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebHunterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
